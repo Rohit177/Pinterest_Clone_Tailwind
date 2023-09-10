ad67a80895fd31f93798e6b7b7acda01e6173236
Rohit Jadhav <34981708+Rohit177@users.noreply.github.com>
2023-09-10 17:30:41 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      boxShadow: {
+        '3xl': '0 0 60px -15px rgba(0, 0, 0, 2)',
+      }
+    },
+  },
+  plugins: [],
+}
+
